9ceb1cb73f882beb74e2dbad1fb42502412aed88
Rafal Sladek <rafal.sladek@gmail.com>
2014-10-31 16:14:20 +0100

    maked it parametrized from outside

--- pullCodeFromRepos.ps1
+++ pullCodeFromRepos.ps1
@@ -1,3 +1,8 @@
+param(
+	[string]$repoBaseDir = 'D:\Sources',
+	[string]$targetZipDir = 'G:\AS24\Code'
+)
+
 Function ListAllRepoDirs ($repoBaseDir, $folderName){
 	Write-Host 'Trying to find all your' $folderName 'folders...'  -foregroundcolor "magenta"    
     try{      
@@ -44,14 +49,19 @@ Function ZipFiles($targetDir, $repoNames)
 {
     if (-not (test-path "$env:ProgramFiles\7-Zip\7z.exe")) { throw "$env:ProgramFiles\7-Zip\7z.exe needed" } 
     set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"     
+	Write-host $sz
 	Write-Host 'Zipping all sources' $folderName 'folders...'  -foregroundcolor "blue"   
     foreach ($repo in $repoNames) { 
          $targetDirName = $repo.Name
 		 $archiveName = Join-Path $targetDir $targetDirName         
          $filesToZip = $repo.FullName 
 		 $archiveName	= "$archiveName.7z"	
-         Write-Host $filesToZip "  ---->" $archiveName	
-         sz u -t7z -mx9 -ms=off  "$archiveName" "$filesToZip" -xr!bin -xr!obj #| FIND /V "ing " | FIND /V "Igor Pavlov"      
+         Write-Host $filesToZip "  ---->" $archiveName			 
+         if (Test-Path $archiveName) {
+			sz u "$archiveName" "$filesToZip"
+		 }Else{
+			sz a -t7z -mx9 -ms=off  -xr!bin -xr!obj -x!.pdb "$archiveName" "$filesToZip" 
+		 }	       
     } 	
 }
 
@@ -73,20 +83,21 @@ Function CleanWithMSBuild($listOfCsProj){
     }
 }
 
-clear
-$gitFolderName = '.git'
-$svnFolderName = '.svn'
-$repoBaseDir = 'D:\Sources'
-#$targetZipDir = 'D:\backup\Code'
-$targetZipDir = 'G:\AS24\Code'
+Function Main($repoBaseDir, $targetZipDir){
+	clear
+	$gitFolderName = '.git'
+	$svnFolderName = '.svn'
 
-#$csprojs = ListAllCsProj $repoBaseDir
-#CleanWithMSBuild $csprojs
-$svnRepos = ListAllRepoDirs $repoBaseDir $svnFolderName
-$gitRepos = ListAllRepoDirs $repoBaseDir $gitFolderName
+	#$csprojs = ListAllCsProj $repoBaseDir
+	#CleanWithMSBuild $csprojs
+	$svnRepos = ListAllRepoDirs $repoBaseDir $svnFolderName
+	$gitRepos = ListAllRepoDirs $repoBaseDir $gitFolderName
 
-UpdateSVNRepos $svnRepos 
-UpdateGitRepos $gitRepos 
+	UpdateSVNRepos $svnRepos 
+	UpdateGitRepos $gitRepos 
 
-ZipFiles $targetZipDir $svnRepos 
-ZipFiles $targetZipDir $gitRepos
+	ZipFiles $targetZipDir $svnRepos 
+	ZipFiles $targetZipDir $gitRepos	
+}
+
+Main $repoBaseDir $targetZipDir
